8179596247e014b7ef47b2d2061576bf84022ed6
ancai <ancaiu9@gmail.com>
2017-03-01 10:42:09 +0800

    解决 热更新不生效 的问题

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ import codePush from 'react-native-code-push';
 import Main from './js/Main';
 
 //Android 和 IOS 应用的入口
-let codePushOptions = {
-	checkFrequency: codePush.CheckFrequency.ON_APP_RESUME
-};
 export default function() {
-	return codePush(codePushOptions)(Main);
+	return codePush(Main);
 }
